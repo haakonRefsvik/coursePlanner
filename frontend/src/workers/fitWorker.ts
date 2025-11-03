aa796e6e9a3c82fbb650dcd5b2f881ae3ca769fd
Håkon Bjørgen Refsvik <hakon.refsvik@me.com>
2025-11-03 21:55:29 +0100

    vite path

--- frontend/src/workers/fitWorker.ts
+++ frontend/src/workers/fitWorker.ts
@@ -2,7 +2,6 @@ import { fitParties } from "../utils/fitParties";
 
 self.onmessage = async (e) => {
   const events = e.data;
-  console.log("jobber");
   const result = await fitParties(events);
   postMessage(result);
 };
