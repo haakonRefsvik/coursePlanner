5f209a10f3d791e8d2e17508d67ec5923726e5cd
Håkon <hakonrefsvik@Hakon-sin-MacBook-Air.local>
2026-01-13 16:38:35 +0100

    removed ping.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -114,7 +114,6 @@ app.get("/health", (_req, res) => {
   res.status(200).send("OK");
 });
 
-
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () =>
   console.log(`Backend running on http://localhost:${PORT}`)
